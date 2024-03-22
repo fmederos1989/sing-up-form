496feaff380aa7fd71f4951d76ba8d2cf5bd8290
Fede Mederos <66494706+fmederos1989@users.noreply.github.com>
2024-03-21 21:36:28 -0300

    actualizando

--- script.js
+++ script.js
@@ -7,24 +7,19 @@ let aviso = document.createElement('p')
 const divPass = document.querySelector('.password')
 
 createAcount.addEventListener('click', function () {
-    if (password.value != confPassword.value) {
-      pwdInputs.forEach((pwdInput) =>{
-        pwdInput.style.border = "1px solid red"
-      })
-      password.value = '';
-      confPassword.value = '';
-      divPass.appendChild(aviso);
-      aviso.textContent = '* Las contraseñas no coiciden';
-      aviso.style.color = 'red';
-
-    } else {
-      alert("Registrado Corretamente");
-      inputs.forEach((input) => {
-        input.value = ''  
-      })
+        if (password.value != confPassword.value) {
+          pwdInputs.forEach((pwdInput) =>{
+          pwdInput.style.border = "1px solid red"
+          })
+          password.value = '';
+          confPassword.value = '';
+          divPass.appendChild(aviso);
+          aviso.textContent = '* Las contraseñas no coiciden';
+          aviso.style.color = 'red';
+        } else {
       divPass.removeChild(aviso);
       pwdInputs.forEach((pwdInput) =>{
-        pwdInput.style.border = '1px solid rgb(118, 118, 118)'
+      pwdInput.style.border = '1px solid rgb(118, 118, 118)'
       })
     }
-});
+  });
